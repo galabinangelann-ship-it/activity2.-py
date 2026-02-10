23385e01a194e4ee2a0926f1eefb15f5d474692f
galabinangelann-ship-it <galabin.angelann@gmail.com>
2026-02-10 18:22:20 +0800

    Add personal information display in ACTIVITY2.py

---
+++ ACTIVITY2.py
@@ -0,0 +1,15 @@
+fullname = "Angel Ann Galabin"
+age = 19
+height = 1.49
+working = False
+print("\nFullname:", fullname)
+print(type(fullname))
+
+print("\nAge:", age)
+print(type(age))
+
+print("\nHeight in meters:", height)
+print(type(height))
+
+print("\nCurrently working?", working)
+print(type(working))
